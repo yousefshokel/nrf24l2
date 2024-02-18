a02177d2ef6ab4d91d317bb7b8985835e58c508f
yousefshokel <120268712+yousefshokel@users.noreply.github.com>
2024-02-18 14:46:46 +0000

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,7 +4,7 @@ import time      #import time library
 
 import spidev
 
-from nrf24 import NRF24   #import NRF24 library
+from lib_nrf24 import NRF24   #import NRF24 library
 
 
 GPIO.setmode(GPIO.BCM)       # set the gpio mode
