4255640a6a907572a61becb3205ac5cd72f2da91
yousefshokel <120268712+yousefshokel@users.noreply.github.com>
2024-02-19 20:07:47 +0000

    Add files via upload

---
+++ receiver.py
@@ -0,0 +1,42 @@
+# Import the RF24 module
+from pyRF24 import pyRF24
+
+# Create an instance of the RF24 class
+radio = pyRF24()
+
+# Setup the radio module
+radio.begin()
+
+ce_pin = 25  # Replace with your GPIO pin
+csn_pin = 8   # Replace with your GPIO pin
+
+radio.setCEPin(ce_pin)
+radio.setCSNPin(csn_pin)
+
+# Set the radio channel and payload size
+radio.setChannel(0x76)
+radio.setPayloadSize(32)
+
+# Set the address for communication (both on transmitter and receiver)
+address = [0xAB, 0xCD, 0xEF, 0x01, 0x23]
+radio.openWritingPipe(address)
+radio.openReadingPipe(1, address)
+
+# Start listening for incoming data
+radio.startListening()
+
+while True:
+    # Check if there is data available to read
+    if radio.available():
+        # Read the data
+        data = radio.read(32)
+        print("Received data:", data.decode('utf-8'))
+
+# Cleanup when the script ends
+radio.end()
+
+
+
+
+
+
