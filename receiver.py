e5c597178df0dbfbc357ab57ec55220a0d11d6a3
yousefshokel <120268712+yousefshokel@users.noreply.github.com>
2024-02-19 20:50:47 +0000

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -32,8 +32,8 @@ radio_number = bool(
 )
 
 # initialize the nRF24L01 on the spi bus
-if not radio.begin():
-    raise OSError("nRF24L01 hardware isn't responding")
+#if not radio.begin():
+#    raise OSError("nRF24L01 hardware isn't responding")
 
 # set the Power Amplifier level to -12 dBm since this test example is
 # usually run with nRF24L01 transceivers in close proximity of each other
